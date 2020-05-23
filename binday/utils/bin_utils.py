a664172c1d11f315f1005f5a14cfc24b8f69d707
Chehan Ratnasiri <chehan.rat@gmail.com>
2020-05-24 00:04:57 +1000

    Added bin LED turning on task logic.

--- binday/utils/bin_utils.py
+++ binday/utils/bin_utils.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from math import ceil
 from typing import List
+
+from binday.server.models.bin_day import CollectionFrequency, DayIndex
+from binday.utils.common_utils import day_index_to_int
 
 
 def parse_serial_str(data: str) -> List[str]:
@@ -13,3 +18,27 @@ def get_bin_level(bin_height: int, sonar_reading: int = 0) -> int:
 
 def get_bin_level_perc(bin_height: int, sonar_reading: int = 0) -> float:
     return (get_bin_level(bin_height, sonar_reading) / bin_height) * 100
+
+
+def check_binday(
+    dt: datetime, bin_day_index: DayIndex, bin_day_freq: CollectionFrequency
+) -> bool:
+    week_day = dt.weekday()
+
+    if week_day != day_index_to_int(bin_day_index):
+        return False
+
+    first_day = dt.replace(day=1)
+    dom = dt.day
+    adjusted_dom = dom + first_day.weekday()
+
+    week_index = int(ceil(adjusted_dom / 7.0))
+
+    if bin_day_freq == CollectionFrequency.WEEKLY:
+        return True
+    if bin_day_freq == CollectionFrequency.FORTNIGHTLY:
+        return True if week_index in [0, 2] else False
+    if bin_day_freq == CollectionFrequency.MONTHLY:
+        return True if week_index == 0 else False
+
+    return False
--- binday/workers/tasks.py
+++ binday/workers/tasks.py
@@ -1,10 +1,12 @@
+from datetime import datetime, timedelta
+
 from binday.binx.binx import BinActions, BinX
 from binday.boards.board import Board, BoardType
 from binday.boards.uno_r3 import UnoR3
 from binday.server.factories.application import create_application, db
 from binday.server.models.bin_reading import BinReading
 from binday.server.models.my_bin import MyBin
-from binday.utils.bin_utils import get_bin_level, get_bin_level_perc
+from binday.utils.bin_utils import check_binday, get_bin_level, get_bin_level_perc
 
 
 def add_bin_reading_task():
@@ -56,3 +58,49 @@ def add_bin_reading_task():
             db.session.commit()
 
             print(f"Added a new sensor record: {bin_reading_obj}")
+
+
+def turn_on_led_task():
+    app = create_application()
+
+    with app.app_context():
+        my_bin_objs = MyBin.query.order_by("device_name").all()
+
+        prev_board = None
+
+        for my_bin_obj in my_bin_objs:
+            try:
+                board = Board(device_name=my_bin_obj.device_name)
+
+                if my_bin_obj.board_type == BoardType.UNO_R3:
+                    board = UnoR3(device_name=my_bin_obj.device_name)
+
+                if prev_board != board:
+                    board.setup()
+            except Exception as ex:
+                # TODO Better logging.
+                print(ex)
+            else:
+                prev_board = board
+
+                binx = BinX(my_bin_obj.name, my_bin_obj.sonar_id, my_bin_obj.led_id)
+                bin_actions = BinActions(board, binx)
+
+                today = datetime.now()
+                tomorrow = today + timedelta(days=1)
+
+                if check_binday(
+                    today, my_bin_obj.bin_day.day_index, my_bin_obj.bin_day.frequency
+                ):
+                    bin_actions.turn_bin_led_on()
+                    print(
+                        f"Turned on LED for {my_bin_obj}, Collection on: {today.date()}"
+                    )
+
+                if check_binday(
+                    tomorrow, my_bin_obj.bin_day.day_index, my_bin_obj.bin_day.frequency
+                ):
+                    bin_actions.turn_bin_led_on()
+                    print(
+                        f"Turned on LED for {my_bin_obj}, Collection on: {tomorrow.date()}"
+                    )
